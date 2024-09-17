57769f78dec094828fb953b72ca7cc33640c3fab
Mehrshad Norozy <mehrshad.nrz@gmail.com>
2024-09-17 22:56:00 +0330

    debug

--- app/routers/response.py
+++ app/routers/response.py
@@ -93,7 +93,7 @@ async def create_answer(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Answer already exists",
         )
-    created_answer = await crud.create_answer(response.id, answer_data)
+    created_answer = await crud.create_answer(response["id"], answer_data)
     return created_answer
 
 
